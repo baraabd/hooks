c0c5a4ed4f6c5e0b5f813427cf591f48c7898ddf
Bara Abdullatif <bara.abd@yahoo.com>
2021-07-02 11:09:09 +0200

    Change code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [state, setState] = useState({
-    city: "",
-    country: "",
-  });
+  
+  const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
 
   const handleChangeCity = (event) => {
-    setState({
-      ...state, city: event.target.value
-    })
+    setCity(event.target.value)
   }
 
   const handleChangeCountry = (event)=> {
-    setState({
-      ...state, country: event.target.value
-    })
+    setCountry(event.target.value)
   }
 
   return (
 
     <form>
       <div>
-        <input type="text" placeholder="City" value={state.city} onChange={handleChangeCity} />
+        <input type="text" placeholder="City" value={city} onChange={handleChangeCity} />
       </div>
       <div>
-        <input type="text" placeholder="Country" value={state.country} onChange={handleChangeCountry} />
+        <input type="text" placeholder="Country" value={country} onChange={handleChangeCountry} />
       </div>
 
-      <h1>I live in {`${state.city}, ${state.country}`} </h1>
+      <h1>I live in {`${city}, ${country}`} </h1>
     </form>
   );
 };
